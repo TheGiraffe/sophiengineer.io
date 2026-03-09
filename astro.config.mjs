480c47bcb6c4a06cf0ff679605736d6da94b30fb
TheGiraffe <TheGiraffe@users.noreply.github.com>
2026-03-09 18:37:38 -0400

    put CNAME under public instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thegiraffe.github.io',
-    base: '/sophiengineer.io',
+    site: 'https://sophiengineer.io',
     integrations: [
         sanity({
             projectId: "ud7ejusl",
